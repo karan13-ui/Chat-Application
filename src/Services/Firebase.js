a62fe6126b3ac6c5885c7c26ea6bfdc9389377fb
Karan Rustagi <63772539+karan13-ui@users.noreply.github.com>
2020-05-09 17:58:33 +0530

    Update Firebase.js

--- src/Services/Firebase.js
+++ src/Services/Firebase.js
@@ -2,14 +2,7 @@ import React from 'react';
 import * as firebase from 'firebase';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyC4vj4x04WKLj1ffrX57tm232RNJ6ULIqE",
-    authDomain: "webapp-ef989.firebaseapp.com",
-    databaseURL: "https://webapp-ef989.firebaseio.com",
-    projectId: "webapp-ef989",
-    storageBucket: "webapp-ef989.appspot.com",
-    messagingSenderId: "306981058280",
-    appId: "1:306981058280:web:fc528cbdf00cdc003f7b5d",
-    measurementId: "G-E5ETZ5M3HJ"
+
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
